0f774cc6effe843ec1d1c94cc175e22d862286a7
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-02 12:55:25 -0300

    add

--- 3FM2/VetorAleatorio.c
+++ 3FM2/VetorAleatorio.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAMANHO 200000
+#define TAMANHO 200
 
 int main() {
 
-    int V[TAMANHO];
+   int Soma = 0;
 
     srand(time(NULL));
 
-    for(int i = 0;i < TAMANHO; i++) V[i] = rand();
+    for(int i = 0;i < TAMANHO; i++) {
+
+     int temp = rand() % 1000;
+     printf("%d\n , temp");
+     Soma =  Soma + temp;
+
+    }
 
     clock_t T0 = clock();
     for(int i = 0;i < TAMANHO; i++) printf("%d\t", V[i]);
@@ -19,5 +25,7 @@ int main() {
     double TempoCPU = (double)(TF = T0) / CLOCKS_PER_SEC;
 
     printf("TEMPO DE CPU %.3f", TempoCPU);
+    printf("Soma dos numeros: %d\n", Soma);
+    
 
 }
